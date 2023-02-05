d8b6788369b2e843397c9021f70fc80dffb5fa35
breadNbutter42 <97899677+breadNbutter42@users.noreply.github.com>
2023-02-05 02:19:27 -0500

    rewards.json

---
+++ src/composables/useRewarderContract.js
@@ -0,0 +1,38 @@
+import { ethers } from 'ethers'
+import useUser from '@/composables/useUser'
+import abi from '@/abi/rewarder.json'
+import useUserStore from '@/stores/user'
+
+export default (address) => {
+  const { wallet, isAuthenticated } = useUser()
+  const userStore = useUserStore()
+
+  let contract
+  const setContract = (payload) => contract = new ethers.Contract(import.meta.env.VITE_CONTRACT_REWARDER, abi, payload)
+
+  const myDepositedLP = async () => await contract.myDepositedLP().then(response => Number(ethers.utils.formatEther(response)).toFixed(2))
+  const pendingTokens = async (payload) => await contract.pendingTokens(payload ?? address.value).then(response => Number(ethers.utils.formatEther(response)).toFixed(2))
+  const deposit = async (amt) => await contract.deposit(amt)
+  const withdrawMyLPAndRewards = async () => await contract.withdrawMyLPAndRewards()
+  
+  userStore.$onAction(({ name, after }) => {
+    after(() => {
+      const actions = {
+        setUser: () => setContract(wallet.getSigner()),
+        resetUser: () => setContract(wallet)
+      }
+
+      actions[name]?.() || null
+    })
+  })
+
+  isAuthenticated.value ? setContract(wallet.getSigner()) : setContract(wallet)
+
+  return {
+    contract,
+    myDepositedLP,
+    pendingTokens,
+    deposit,
+    withdrawMyLPAndRewards
+  }
+}
